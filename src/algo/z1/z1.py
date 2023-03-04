b5e75de560ba5d4ecaef77d5ff800e8b102a4319
tomo2515x <tomo2515pl@gmail.com>
2023-03-04 15:20:12 +0100

    uo

--- src/algo/z1/z1.py
+++ src/algo/z1/z1.py
@@ -4,8 +4,10 @@ def add(a, b):
 
 def mul(a, b):
     print(a * b)
+
 def calculate(fn, a, b):
-    return fn(a,b)
+    if fn == "add": return add(a,b)
+    return mul(a,b)
 
 print(calculate("add", 1, 2))
 print(calculate("mul", 1, 2))
