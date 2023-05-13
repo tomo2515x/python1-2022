5c3f599e7cc8d2b0744a1ab549aa550b63654d33
tomo2515x <tomo2515pl@gmail.com>
2023-05-13 15:05:56 +0200

    solution

--- src/algo/z7_t1/solution.py
+++ src/algo/z7_t1/solution.py
@@ -16,9 +16,28 @@ jeśli długości *'ek klucza + długości #'ów zamka są dla wszystkich takie 
 
 
 def open_lock(key: list[int], lock: list[int]) -> int:
-    """
-    :param key:   np. [4,1,1,3,1,2]
-    :param lock:  np  [1,4,2,2,4,3]
-    :return:    -1 jeśli wartości są "invalid" (np. <0); 0 jeśli klucz otwiera zamek, 1 jeśli klucz nie otwiera zamka
-    """
-    return 0
+    longest_key = key[1]
+    shortest_lock = lock[1]
+    i = 0
+    match = 0
+
+    for k in range(len(key)):
+        if key[k] > longest_key:
+            longest_key = key[k]
+        if lock[k] < shortest_lock:
+            shortest_lock = lock[k]
+    
+    for teeth in key:
+
+        if teeth + lock[i] != longest_key+shortest_lock:
+            i+=1
+
+        else:
+            i+=1
+            match +=1
+
+    if match == len(lock):
+        print("im opening")
+        return 0
+    print("im not opening")
+    return 1
