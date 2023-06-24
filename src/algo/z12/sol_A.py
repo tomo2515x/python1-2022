ecee3f94b82f2ba734c3d536ddcf449d5c6aeeb2
tomo2515x <tomo2515pl@gmail.com>
2023-06-24 15:12:07 +0200

    Update sol_A.py

--- src/algo/z12/sol_A.py
+++ src/algo/z12/sol_A.py
@@ -1,3 +1,23 @@
+def remove_items(test_list, item):
+ 
+    res = [i for i in test_list if i != item]
+ 
+    return res
+
 def get_min_number_of_operations(a: list[int]) -> int:
-    # todo: your code here
-    return 0
+
+    streak = 0
+    operat = []
+
+    for num_index in range(len(a)):
+
+        if a[num_index] <= 0:
+            streak += 1
+            
+        else: 
+            operat.append(streak)
+            streak = 0
+            
+    operat.append(streak)    
+    return len(remove_items(operat, 0))
+
